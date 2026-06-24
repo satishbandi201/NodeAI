76d5630f5f9e5aa4edd2e67a7fa125d59fc27f8e
Satish Bandi <bandisatish20@gmail.com>
2026-06-24 21:28:08 +0530

    appjs updated for metrics

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
+// const express=require('express');
+
+// const app=express();
+
+// app.get('/',(req,res)=>{
+//     res.send('DevOps Assignment Success Satish Bandi');
+// });
+
+// app.listen(3000,()=>{
+//     console.log('Started');
+// });
+
 const express = require("express");
 const client = require("prom-client");
 
