3cb36364f2e1ebededdf91982ca3ac33983d506d
Satish Bandi <bandisatish20@gmail.com>
2026-06-25 12:30:37 +0530

    metrics added

--- app.js
+++ app.js
@@ -1,66 +1,33 @@
-const express=require('express');
+// const express=require('express');
 
-const app=express();
+// const app=express();
 
-app.get('/',(req,res)=>{
+// app.get('/',(req,res)=>{
+//     res.send('DevOps Assignment Success Satish Bandi');
+// });
+
+// app.listen(3000,()=>{
+//     console.log('Started');
+// });
+
+const express = require('express');
+const client = require('prom-client');
+
+const app = express();
+
+// Collect default Node.js metrics
+client.collectDefaultMetrics();
+
+app.get('/', (req, res) => {
     res.send('DevOps Assignment Success Satish Bandi');
 });
 
-app.listen(3000,()=>{
-    console.log('Started');
+// Prometheus metrics endpoint
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', client.register.contentType);
+    res.end(await client.register.metrics());
 });
 
-// const express = require("express");
-// const client = require("prom-client");
-
-// const app = express();
-
-// /* -------------------- PROMETHEUS SETUP -------------------- */
-
-// // Create custom registry
-// const register = new client.Registry();
-
-// // Collect default Node.js metrics
-// client.collectDefaultMetrics({ register });
-
-// /* -------------------- CUSTOM METRICS -------------------- */
-
-// // HTTP request counter
-// const httpRequestsTotal = new client.Counter({
-//   name: "http_requests_total",
-//   help: "Total HTTP requests",
-//   labelNames: ["method", "route", "status"]
-// });
-
-// // IMPORTANT: register metric with Prometheus registry
-// register.registerMetric(httpRequestsTotal);
-
-// /* -------------------- MIDDLEWARE -------------------- */
-
-// // Track all incoming requests
-// app.use((req, res, next) => {
-//   res.on("finish", () => {
-//     httpRequestsTotal
-//       .labels(req.method, req.path, res.statusCode)
-//       .inc();
-//   });
-//   next();
-// });
-
-// /* -------------------- ROUTES -------------------- */
-
-// app.get("/", (req, res) => {
-//   res.send("NodeAI Assignment is running");
-// });
-
-// /* Metrics endpoint for Prometheus */
-// app.get("/metrics", async (req, res) => {
-//   res.set("Content-Type", register.contentType);
-//   res.end(await register.metrics());
-// });
-
-// /* -------------------- SERVER -------------------- */
-
-// app.listen(3000, () => {
-//   console.log("Server running on port 3000");
-// });
+app.listen(3000, () => {
+    console.log('Started');
+});
